c72c97c04da049162de7ab75e8fec397944df03d
ssw331 <2405353202@qq.com>
2023-09-25 23:31:40 +0800

    config changed.

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -10,11 +10,11 @@ const CONFIG = {
   ],
 
   // 英雄区(首页顶部大卡)
-  HERO_TITLE_1: '分享编程',
-  HERO_TITLE_2: '与思维认知',
-  HERO_TITLE_3: 'TANGLY1024.COM',
-  HERO_TITLE_4: '新版上线',
-  HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
+  HERO_TITLE_1: '但行好事',
+  HERO_TITLE_2: '莫问前程',
+  HERO_TITLE_3: 'sswblog.netlify.app',
+  HERO_TITLE_4: '早上中午晚上好',
+  HERO_TITLE_5: '我的朋友',
   HERO_TITLE_LINK: 'https://sswblog.netlify.app',
 
   // 英雄区显示三个置顶分类
@@ -36,42 +36,42 @@ const CONFIG = {
     '🤖️ 数码科技爱好者',
     '🧱 团队小组发动机'
   ],
-  INFO_CARD_URL: 'https://github.com/ssw331/NotionNext', // 个人资料底部按钮链接
+  INFO_CARD_URL: 'https://github.com/ssw331/', // 个人资料底部按钮链接
 
   // 用户技能图标
   GROUP_ICONS: [
+    // {
+    //   title_1: 'AfterEffect',
+    //   img_1: '/images/heo/20239df3f66615b532ce571eac6d14ff21cf072602.webp',
+    //   color_1: '#989bf8',
+    //   title_2: 'Sketch',
+    //   img_2: '/images/heo/2023e0ded7b724a39f12d59c3dc8fbdc7cbe074202.webp',
+    //   color_2: '#ffffff'
+    // },
     {
-      title_1: 'AfterEffect',
-      img_1: '/images/heo/20239df3f66615b532ce571eac6d14ff21cf072602.webp',
-      color_1: '#989bf8',
-      title_2: 'Sketch',
-      img_2: '/images/heo/2023e0ded7b724a39f12d59c3dc8fbdc7cbe074202.webp',
-      color_2: '#ffffff'
+      // title_1: 'Docker',
+      // img_1: '/images/heo/20231108a540b2862d26f8850172e4ea58ed075102.webp',
+      // color_1: '#57b6e6',
+      // title_2: 'Photoshop',
+      // img_2: '/images/heo/2023e4058a91608ea41751c4f102b131f267075902.webp',
+      // color_2: '#4082c3'
     },
     {
-      title_1: 'Docker',
-      img_1: '/images/heo/20231108a540b2862d26f8850172e4ea58ed075102.webp',
-      color_1: '#57b6e6',
-      title_2: 'Photoshop',
-      img_2: '/images/heo/2023e4058a91608ea41751c4f102b131f267075902.webp',
-      color_2: '#4082c3'
-    },
-    {
-      title_1: 'FinalCutPro',
-      img_1: '/images/heo/20233e777652412247dd57fd9b48cf997c01070702.webp',
-      color_1: '#ffffff',
+      title_1: 'Git',
+      img_1: '/images/heo/2023ffa5707c4e25b6beb3e6a3d286ede4c6071102.webp',
+      color_1: '#df5b40',
       title_2: 'Python',
       img_2: '/images/heo/20235c0731cd4c0c95fc136a8db961fdf963071502.webp',
       color_2: '#ffffff'
     },
-    {
-      title_1: 'Swift',
-      img_1: '/images/heo/202328bbee0b314297917b327df4a704db5c072402.webp',
-      color_1: '#eb6840',
-      title_2: 'Principle',
-      img_2: '/images/heo/2023f76570d2770c8e84801f7e107cd911b5073202.webp',
-      color_2: '#8f55ba'
-    },
+    // {
+    //   title_1: 'Swift',
+    //   img_1: '/images/heo/202328bbee0b314297917b327df4a704db5c072402.webp',
+    //   color_1: '#eb6840',
+    //   title_2: 'Principle',
+    //   img_2: '/images/heo/2023f76570d2770c8e84801f7e107cd911b5073202.webp',
+    //   color_2: '#8f55ba'
+    // },
     {
       title_1: 'illustrator',
       img_1: '/images/heo/20237359d71b45ab77829cee5972e36f8c30073902.webp',
@@ -89,16 +89,16 @@ const CONFIG = {
       color_2: '#e9572b'
     },
     {
-      title_1: 'Git',
-      img_1: '/images/heo/2023ffa5707c4e25b6beb3e6a3d286ede4c6071102.webp',
-      color_1: '#df5b40',
-      title_2: 'Rhino',
-      img_2: '/images/heo/20231ca53fa0b09a3ff1df89acd7515e9516173302.webp',
-      color_2: '#1f1f1f'
+      //   title_1: 'FinalCutPro',
+      //   img_1: '/images/heo/20233e777652412247dd57fd9b48cf997c01070702.webp',
+      //   color_1: '#ffffff',
+      // title_2: 'Rhino',
+      // img_2: '/images/heo/20231ca53fa0b09a3ff1df89acd7515e9516173302.webp',
+      // color_2: '#1f1f1f'
     }
   ],
 
-  SOCIAL_CARD: true, // 是否显示右侧，点击加入社群按钮
+  SOCIAL_CARD: false, // 是否显示右侧，点击加入社群按钮
   SOCIAL_CARD_TITLE_1: '交流频道',
   SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
   SOCIAL_CARD_TITLE_3: '点击加入社群',
